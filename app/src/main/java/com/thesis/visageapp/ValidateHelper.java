01402eda34af49a0eb3634ce3c37ea1f639826ee
Agata Guli <agataguli@gmail.com>
2017-12-03 20:52:30 +0100

    Validate fixes

--- app/src/main/java/com/thesis/visageapp/ValidateHelper.java
+++ app/src/main/java/com/thesis/visageapp/ValidateHelper.java
@@ -2,15 +2,12 @@ package com.thesis.visageapp;
 
 import android.text.TextUtils;
 
-import java.util.regex.Pattern;
-
 public class ValidateHelper {
 
 
     public static boolean isValidLogin(String s) {
         return !s.isEmpty() && s.length() > 5 && s.length() < 17;
     }
-
 
     public static boolean isValidPassword(String s) {
         return !s.isEmpty() && s.length() > 7 && s.length() < 17;
@@ -27,14 +24,9 @@ public class ValidateHelper {
     public static boolean isValidNameSurname(String s) {
         return !s.isEmpty() && s.length() > 2 && s.length() < 16;
     }
-    public static boolean isValidEmail(String email){
 
-        return Pattern.compile("^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
-                + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
-                + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
-                + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
-                + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
-                + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$").matcher(email).matches();
+    public static boolean isValidEmail(String email){
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
     public static boolean isValidPhoneNumber(String phoneNumber) {
@@ -43,11 +35,10 @@ public class ValidateHelper {
 
     public static boolean isValidCountry(String country) {
         return !country.isEmpty() && country.length() > 2 && country.length() < 20;
-        //TODO zamien to na dropdown
+        // TODO change countries to enum class, enable by dropdown
     }
 
     public static boolean isValidPostCode(String postCode) {
-        return !postCode.isEmpty() && postCode.length() > 3 && postCode.length() < 8 && postCode.matches("[0-9]");
-        //TODO dokladniejsza walidacja kodu pocztowego
+        return !postCode.matches("\\d{2}-\\d{3}");
     }
 }
