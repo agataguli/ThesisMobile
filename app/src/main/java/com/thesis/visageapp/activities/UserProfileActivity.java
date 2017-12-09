81534ca27a79a1614a4d2755dd96699d74d8a668
Agata Guli <agataguli@gmail.com>
2017-12-09 12:28:55 +0100

    M010 User profile/account - basic frontend

--- app/src/main/java/com/thesis/visageapp/activities/UserProfileActivity.java
+++ app/src/main/java/com/thesis/visageapp/activities/UserProfileActivity.java
@@ -2,6 +2,8 @@ package com.thesis.visageapp.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,8 +16,10 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class UserProfileActivity extends AppCompatActivity {
+    private static final String TAG = "UserProfileActivity";
     private User user = new User();
-    Bundle extras = new Bundle();
+    private Bundle extras = new Bundle();
+    private boolean onlyPreview = true;
 
     @Bind(R.id.textview_login_u)
     TextView loginTextView;
@@ -47,6 +51,8 @@ public class UserProfileActivity extends AppCompatActivity {
     Button changePasswordButton;
     @Bind(R.id.button_user_delete_account_u)
     Button deleteAccountButton;
+    @Bind(R.id.textview_hint_u)
+    TextView hintTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,12 +61,119 @@ public class UserProfileActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         this.extras = getIntent().getExtras();
-        if(this.extras != null) {
+        if (this.extras != null) {
             this.user = RequestResponseHelper.processUserStringJSON(extras.getString(
                     RequestResponseHelper.USER_BUNDLE));
             this.prepareFormFromUser();
         }
+
+        this.changeCredentialsButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                changeBasicUserData();
+            }
+        });
+
+        this.changePasswordButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                changeUserPassword();
+            }
+        });
+
+        this.deleteAccountButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                deleteUserAccount();
+            }
+        });
     }
+
+    private void deleteUserAccount() {
+        Log.d(TAG, "Removing user account processed");
+        this.setButtonsEnable(this.deleteAccountButton);
+        this.setPasswordInputsVisibleAndEnable(!this.onlyPreview);
+    }
+
+    private void changeUserPassword() {
+        Log.d(TAG, "Changing password processed");
+        this.setButtonsEnable(this.changePasswordButton);
+        this.setPasswordInputsVisibleAndEnable(!this.onlyPreview);
+    }
+
+    private void changeBasicUserData() {
+        Log.d(TAG, "Changing credentials processed");
+        this.setBasicTextInputsEnable(this.onlyPreview);
+        this.setButtonsEnable(this.changeCredentialsButton);
+        this.setPasswordInputsVisibleAndEnable(!this.onlyPreview);
+    }
+
+    private void setButtonsEnable(Button pressedButton) {
+        this.onlyPreview = !this.onlyPreview;
+        if (this.onlyPreview) {
+            this.setButtonsEnable(true, true, true);
+            this.restoreTextOfButtons();
+            this.hintTextView.setText(this.getResources().getString(R.string.text_hint_default));
+        } else {
+            if (pressedButton == this.changeCredentialsButton) {
+                this.setButtonsEnable(true, false, false);
+                this.hintTextView.setText(this.getResources().getString(R.string.text_hint_change_credentials_insert_password));
+                this.changeCredentialsButton.setText(this.getResources().getString(R.string.save));
+            } else {
+                if (pressedButton == this.changePasswordButton) {
+                    this.setButtonsEnable(false, true, false);
+                    this.hintTextView.setText(this.getResources().getString(R.string.text_hint_change_password));
+                    this.changePasswordButton.setText(this.getResources().getString(R.string.save));
+                } else {
+                    this.setButtonsEnable(false, false, true);
+                    this.hintTextView.setText(this.getResources().getString(R.string.text_hint_delete_acccount));
+                    this.deleteAccountButton.setText(this.getResources().getString(R.string.save));
+                }
+            }
+        }
+    }
+
+    private void restoreTextOfButtons() {
+        this.changeCredentialsButton.setText(this.getResources().getString(R.string.changeCredentials));
+        this.changePasswordButton.setText(this.getResources().getString(R.string.changePassword));
+        this.deleteAccountButton.setText(this.getResources().getString(R.string.deleteAccount));
+    }
+
+    private void setButtonsEnable(boolean b1, boolean b2, boolean b3) {
+        this.changeCredentialsButton.setEnabled(b1);
+        this.changePasswordButton.setEnabled(b2);
+        this.deleteAccountButton.setEnabled(b3);
+    }
+
+    private void setPasswordInputsVisibleAndEnable(boolean enable) {
+        int visible = View.GONE;
+        if(enable) visible = View.VISIBLE;
+        this.passwordText.setVisibility(visible);
+        this.passwordText.setEnabled(enable);
+        this.rePasswordText.setVisibility(visible);
+        this.passwordText.setEnabled(enable);
+        this.passwordText.setText(null);
+        this.rePasswordText.setText(null);
+    }
+
+    private void setBasicTextInputsEnable(boolean val) {
+        loginTextView.setEnabled(val);
+        nameText.setEnabled(val);
+        surnameText.setEnabled(val);
+        emailText.setEnabled(val);
+        phoneNumberText.setEnabled(val);
+        countryText.setEnabled(val);
+        postCodeText.setEnabled(val);
+        cityText.setEnabled(val);
+        streetText.setEnabled(val);
+        addressDetailsText.setEnabled(val);
+    }
+
+    //ValidateHelper.validateUserData(peselText, loginText, passwordText, rePasswordText, nameText, surnameText,
+    //emailText, phoneNumberText, countryText, postCodeText, cityText, streetText, addressDetailsText, this))
 
     private void prepareFormFromUser() {
         loginTextView.setText(user.getLogin());
