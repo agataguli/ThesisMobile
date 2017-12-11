5f093c7cead4d225849ac4edb0984ea0d1c6e602
Agata Guli <agataguli@gmail.com>
2017-12-12 00:51:33 +0100

    Pushing imageRequest method to productDetails implementation on the another branch

--- app/src/main/java/com/thesis/visageapp/processors/VolleyRequestProcessor.java
+++ app/src/main/java/com/thesis/visageapp/processors/VolleyRequestProcessor.java
@@ -1,12 +1,15 @@
 package com.thesis.visageapp.processors;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.google.gson.Gson;
 import com.thesis.visageapp.R;
@@ -23,6 +26,21 @@ import java.security.NoSuchAlgorithmException;
 
 public class VolleyRequestProcessor {
     private String responseCode = " 702";
+
+    public static void processImageRequest(final ImageView iv, String productPhotoLink) {
+        ImageRequest ir = new ImageRequest(productPhotoLink, new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap response) {
+                iv.setImageBitmap(response);
+            }
+        }, 300, 300, null, null, new Response.ErrorListener(){
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // do not send any notification if imageRequest not response
+            }
+        });
+    }
 
     // TODO: move the logic here
     public String processPostUserData(final String URL, User user, final Context context) throws JSONException, UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException {
